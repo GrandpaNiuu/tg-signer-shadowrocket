bd80d149f1fc3189b7657fae0c9b40c83d0b5788
GrandpaNiuu <q3j1h8@gmail.com>
2026-07-21 12:18:04 +0800

    Load platform account health center from the security bootstrap

--- admin/src/same-origin-write.js
+++ admin/src/same-origin-write.js
@@ -23,4 +23,6 @@ globalThis.fetch = function sameOriginAdminFetch(input, init = {}) {
   return nativeFetch(input, { ...init, headers });
 };
 
+void import("./platform-account-health.js");
+
 export const __test = { requestMethod, requestUrl };
